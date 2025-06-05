cb924675e49a03e02ad5ca2459a3a824a4e3da37
Nilesh Chavan <cnilesh123.nc@gmail.com>
2025-06-05 18:36:31 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/sections/ContactForm.tsx
+++ src/components/sections/ContactForm.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { useFormState, useFormStatus } from 'react-dom';
+import { useActionState } from 'react';
+import { useFormStatus } from 'react-dom';
 import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -31,7 +32,7 @@ function SubmitButton() {
 }
 
 export default function ContactForm() {
-  const [state, formAction] = useFormState(submitContactForm, initialState);
+  const [state, formAction] = useActionState(submitContactForm, initialState);
   const { toast } = useToast();
 
   const form = useForm<ContactFormValues>({
